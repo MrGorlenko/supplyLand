5bc22a6cae0083a07e1ba5e8ab5cb876ce38a725
MrGorlenko <52908127+MrGorlenko@users.noreply.github.com>
2020-06-22 23:49:12 +0300

    text

--- js/mobile/swiper.js
+++ js/mobile/swiper.js
@@ -10,7 +10,11 @@ $(document).ready(function(){
         
       })
 
-    let arr = [$('.swiper__button_1'), $('.swiper__button_2'), $('.swiper__button_3'), $('.swiper__button_4'), $('.swiper__button_5')];
+    let arr = [$('.swiper__button_1'), $('.swiper__button_2'), $('.swiper__button_3'), $('.swiper__button_4'), $('.swiper__button_5')],
+
+    aboutConent = [$('.about-content-1'), $('.about-content-2'), $('.about-content-3'), $('.about-content-4'), $('.about-content-5')];
+
+    // $('#about-content').innerHTML = arrText[0]
 
     arr[1].click(function(){
         arr[0].removeClass('swiper__button_active')
@@ -18,6 +22,12 @@ $(document).ready(function(){
         arr[2].removeClass('swiper__button_active')
         arr[3].removeClass('swiper__button_active')
         arr[4].removeClass('swiper__button_active')
+
+        aboutConent[1].removeClass('about-content_none');
+        aboutConent[0].addClass('about-content_none');
+        aboutConent[2].addClass('about-content_none');
+        aboutConent[3].addClass('about-content_none');
+        aboutConent[4].addClass('about-content_none');
     })
 
     arr[0].click(function(){
@@ -26,6 +36,12 @@ $(document).ready(function(){
         arr[2].removeClass('swiper__button_active')
         arr[3].removeClass('swiper__button_active')
         arr[4].removeClass('swiper__button_active')
+
+        aboutConent[0].removeClass('about-content_none');
+        aboutConent[1].addClass('about-content_none');
+        aboutConent[2].addClass('about-content_none');
+        aboutConent[3].addClass('about-content_none');
+        aboutConent[4].addClass('about-content_none');
     })
 
     arr[2].click(function(){
@@ -34,6 +50,12 @@ $(document).ready(function(){
         arr[1].removeClass('swiper__button_active')
         arr[3].removeClass('swiper__button_active')
         arr[4].removeClass('swiper__button_active')
+
+        aboutConent[2].removeClass('about-content_none');
+        aboutConent[1].addClass('about-content_none');
+        aboutConent[0].addClass('about-content_none');
+        aboutConent[3].addClass('about-content_none');
+        aboutConent[4].addClass('about-content_none');
     })
 
     arr[3].click(function(){
@@ -42,6 +64,12 @@ $(document).ready(function(){
         arr[2].removeClass('swiper__button_active')
         arr[0].removeClass('swiper__button_active')
         arr[4].removeClass('swiper__button_active')
+
+        aboutConent[3].removeClass('about-content_none');
+        aboutConent[1].addClass('about-content_none');
+        aboutConent[2].addClass('about-content_none');
+        aboutConent[0].addClass('about-content_none');
+        aboutConent[4].addClass('about-content_none');
     })
 
     arr[4].click(function(){
@@ -50,14 +78,15 @@ $(document).ready(function(){
         arr[2].removeClass('swiper__button_active')
         arr[3].removeClass('swiper__button_active')
         arr[0].removeClass('swiper__button_active')
+
+        aboutConent[4].removeClass('about-content_none');
+        aboutConent[1].addClass('about-content_none');
+        aboutConent[2].addClass('about-content_none');
+        aboutConent[3].addClass('about-content_none');
+        aboutConent[0].addClass('about-content_none');
     })
 
 
-    // $(`.swiper__button_3`).click(function(){
 
-    //     $(this).addClass('swiper__button_active');
-    //     $('.swiper__button_1').removeClass('swiper__button_active');
-
-    // })
 
 })
