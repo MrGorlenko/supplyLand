c39d1bd156f72ae8c6bfc2d34aea97f696d547d1
MrGorlenko <52908127+MrGorlenko@users.noreply.github.com>
2020-06-22 23:18:06 +0300

    buttons slider

--- js/mobile/swiper.js
+++ js/mobile/swiper.js
@@ -10,4 +10,30 @@ $(document).ready(function(){
         
       })
 
+    let arr = [$('.swiper__button_1'), $('.swiper__button_2'), $('.swiper__button_3'), $('.swiper__button_4'), $('.swiper__button_5')];
+
+    arr[1].click(function(){
+        arr[0].removeClass('swiper__button_active')
+        arr[1].addClass('swiper__button_active')
+        arr[2].removeClass('swiper__button_active')
+        arr[3].removeClass('swiper__button_active')
+        arr[4].removeClass('swiper__button_active')
+    })
+
+    arr[0].click(function(){
+        arr[1].removeClass('swiper__button_active')
+        arr[0].addClass('swiper__button_active')
+        arr[2].removeClass('swiper__button_active')
+        arr[3].removeClass('swiper__button_active')
+        arr[4].removeClass('swiper__button_active')
+    })
+
+
+    // $(`.swiper__button_3`).click(function(){
+
+    //     $(this).addClass('swiper__button_active');
+    //     $('.swiper__button_1').removeClass('swiper__button_active');
+
+    // })
+
 })
