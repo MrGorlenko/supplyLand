4183fc93204c7fe3938a6b5e73eac3861a28b8e5
MrGorlenko <52908127+MrGorlenko@users.noreply.github.com>
2020-06-22 22:37:49 +0300

    slider buttons init

---
+++ js/mobile/swiper.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    var mySwiper = new Swiper ('.swiper-container', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        initialSlide: 0,
+        slidesPerView: 2.5,
+        spaceBetween: 12
+        
+      })
+
+})
